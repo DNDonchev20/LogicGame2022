fbf8a246837f6753335b035147d26caecc59ca3f
TSTranev20 <81413006+TSTranev20@users.noreply.github.com>
2022-05-12 18:04:12 +0300

    Make unit tests for addCard functions. Close #31

--- LogicTenacity/UnitTest/UnitTest.cpp
+++ LogicTenacity/UnitTest/UnitTest.cpp
@@ -18,7 +18,87 @@ namespace UnitTesting
 
 			result = check(2, 0, 1);
 			expected = false;
+
+			Assert::AreEqual(result, expected, L"Expected false!");
 		}
+
+		TEST_METHOD(TestCheckFunctionTwo)
+		{
+			bool expected;
+			bool result;
+
+			result = check(3, 3, 4);
+			expected = false;
+
+			Assert::AreEqual(result, expected, L"Expected false!");
+		}
+
+		TEST_METHOD(TestCheckFunctionThree)
+		{
+			bool expected;
+			bool result;
+
+			result = check(4, 1, 2);
+			expected = false;
+
+			Assert::AreEqual(result, expected, L"Expected false!");
+		}
+
+		TEST_METHOD(TestCheckFunctionFour)
+		{
+			bool expected;
+			bool result;
+
+			result = check(2, 1, 0);
+			expected = false;
+
+			Assert::AreEqual(result, expected, L"Expected false!");
+		}
+		
+			TEST_METHOD(TestaddCardFunctionOne)
+		{
+			bool expected;
+			bool result;
+
+			result = addCard(1, 2);
+			expected = false;
+
+			Assert::AreEqual(result, expected, L"Expected false!");
+		}
+
+			TEST_METHOD(TestaddCardFunctionTwo)
+			{
+				bool expected;
+				bool result;
+
+				result = addCard(3, 4);
+				expected = false;
+
+				Assert::AreEqual(result, expected, L"Expected false!");
+			}
+
+			TEST_METHOD(TestaddCardFunctionThree)
+			{
+				bool expected;
+				bool result;
+
+				result = addCard(5, 8);
+				expected = false;
+
+				Assert::AreEqual(result, expected, L"Expected false!");
+			}
+
+			TEST_METHOD(TestaddCardFunctionFour)
+			{
+				bool expected;
+				bool result;
+
+				result = addCard(2, 3);
+				expected = false;
+
+				Assert::AreEqual(result, expected, L"Expected false!");
+			}
+			
 
 	};
 }
